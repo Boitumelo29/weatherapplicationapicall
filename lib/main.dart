50e726f5b5e9afc1f2ca36f78966947857ac26f4
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-09 14:58:04 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapplicationapicall/weather_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [],
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: WeatherPage(),
     );
   }
 }
