fc5cbc505ecc7877df660f098508a582e6cb3fb1
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-07 08:47:41 +0200

    update

--- lib/models.dart
+++ lib/models.dart
@@ -4,6 +4,11 @@ class WeatherResponse {
 
   WeatherResponse({required this.main, required this.description});
 
+  factory WeatherResponse.toJson(Map<String, dynamic> json) {
+    var weather = json['weather'];
+    return WeatherResponse(main: weather[''], description: weather['']);
+  }
+
   @override
   String toString() {
     return 'WeatherResponse(description: $description, main: $main)';
